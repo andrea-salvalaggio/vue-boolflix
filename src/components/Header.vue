253b832dba9d16c6968e608619dfaa61586e531b
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-21 13:13:19 +0200

    creo input di ricerca con bottone

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,13 @@
 <template>
-    <header>
+    <header class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Recipient's username">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                </div>
+            </div>
+        </div>
         
     </header>
 </template>
